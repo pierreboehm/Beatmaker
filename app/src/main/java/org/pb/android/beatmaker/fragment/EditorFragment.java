6a9428ba01b8ddb46d1bb80d15e98be14d61c5d1
pierreboehm <pierre.boehm@web.de>
2021-12-03 17:46:14 +0100

    implement editor fragment class

--- app/src/main/java/org/pb/android/beatmaker/fragment/EditorFragment.java
+++ app/src/main/java/org/pb/android/beatmaker/fragment/EditorFragment.java
@@ -1,13 +1,42 @@
 package org.pb.android.beatmaker.fragment;
 
+import android.annotation.SuppressLint;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+import org.pb.android.beatmaker.R;
+import org.pb.android.beatmaker.data.ContentTickContainer;
+import org.pb.android.beatmaker.data.ContentTickContainer_;
 
-@EFragment
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressLint("NonConstantResourceId")
+@EFragment(R.layout.fragment_editor)
 public class EditorFragment extends Fragment {
 
-    private static final String TAG = EditorFragment.class.getSimpleName();
+    public static final String TAG = EditorFragment.class.getSimpleName();
+
+    @ViewById(R.id.contentSamplesContainer)
+    ViewGroup contentSamplesContainer;
+
+    private List<ContentTickContainer> contentTickContainerList = new ArrayList<>();
 
     // TODO
+    @AfterViews
+    public void initViews() {
+        setupContentTickHolders();
+    }
+
+    private void setupContentTickHolders() {
+        for (int index = 0; index <= 31; index++) {
+            ContentTickContainer tickContainer = ContentTickContainer_.build(getContext(), index);
+            contentSamplesContainer.addView(tickContainer);
+            contentTickContainerList.add(tickContainer);
+        }
+    }
 }

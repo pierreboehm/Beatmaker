86fc0b0bc214293b120eebaec975267585d921d3
pierreboehm <pierre.boehm@web.de>
2021-12-03 17:45:39 +0100

    implement clickable view class

---
+++ app/src/main/java/org/pb/android/beatmaker/fragment/ui/ClickableImageButton.java
@@ -0,0 +1,67 @@
+package org.pb.android.beatmaker.fragment.ui;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+
+import android.util.AttributeSet;
+
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.androidannotations.annotations.Bean;
+import org.androidannotations.annotations.EViewGroup;
+import org.androidannotations.annotations.ViewById;
+import org.pb.android.beatmaker.R;
+import org.pb.android.beatmaker.sound.SoundManager;
+
+@SuppressLint("NonConstantResourceId")
+@EViewGroup(R.layout.button_clickable_imageview)
+public class ClickableImageButton extends FrameLayout {
+
+    private static final String TAG = ClickableImageButton.class.getSimpleName();
+
+    public enum Types {
+        KICK, SNARE, HIHAT, TONE
+    }
+
+    @ViewById
+    ImageView ivImage;
+
+    @Bean
+    SoundManager soundManager;
+
+    private boolean on;
+
+    public ClickableImageButton(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setImage(int resourceId) {
+        ivImage.setImageResource(resourceId);
+    }
+
+    public void setState(Types type) {
+        on = !on;
+
+        if (on) {
+            soundManager.playSound(type);
+        }
+
+        switch (type) {
+            case KICK:
+                ivImage.setBackgroundResource(on ? R.drawable.rounded_background_green : R.drawable.rounded_background_blue);
+                break;
+            case SNARE:
+                ivImage.setBackgroundResource(on ? R.drawable.rounded_background_red : R.drawable.rounded_background_blue);
+                break;
+            case HIHAT:
+                ivImage.setBackgroundResource(on ? R.drawable.rounded_background_yellow : R.drawable.rounded_background_blue);
+                break;
+            case TONE:
+                break;
+        }
+    }
+}
